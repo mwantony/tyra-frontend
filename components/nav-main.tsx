540502cb038a0c4b49dc2c44d04a23416ee2c560
Antony Maziero Warken <againplayi7@gmail.com>
2025-04-19 17:46:10 -0300

    .

--- components/nav-main.tsx
+++ components/nav-main.tsx
@@ -30,6 +30,7 @@ export function NavMain({
           <Link href={"/vendas/adicionar"}>
             <SidebarMenuItem className="flex items-center gap-2">
               <SidebarMenuButton
+                variant="outline"
                 tooltip="Quick Create"
                 className="min-w-8 bg-primary text-primary-foreground duration-200 ease-linear hover:bg-primary/90 hover:text-primary-foreground active:bg-primary/90 active:text-primary-foreground"
               >
