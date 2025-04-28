4e8e23dbba9c555bf203cb53bf69c41ca0c4cde8
Antony Maziero Warken <againplayi7@gmail.com>
2025-04-28 11:32:18 -0300

    .

--- components/nav-secondary.tsx
+++ components/nav-secondary.tsx
@@ -11,6 +11,7 @@ import {
   SidebarMenuItem,
 } from "@/components/ui/sidebar";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export function NavSecondary({
   items,
@@ -23,6 +24,7 @@ export function NavSecondary({
   }[];
 } & React.ComponentPropsWithoutRef<typeof SidebarGroup>) {
   const { isMobile, setOpenMobile } = useSidebar(); // Obtenha as funções do sidebar
+  const pathname = usePathname();
 
   const handleItemClick = () => {
     if (isMobile) {
@@ -34,18 +36,33 @@ export function NavSecondary({
     <SidebarGroup {...props}>
       <SidebarGroupContent>
         <SidebarMenu>
-          {items.map((item) => (
-            <SidebarMenuItem key={item.title}>
-              <Link href={item.url} onClick={handleItemClick} className="w-full">
-                <SidebarMenuButton asChild>
-                  <div className="flex items-center gap-2 w-full">
-                    <item.icon className="h-4 w-4" />
-                    <span>{item.title}</span>
-                  </div>
-                </SidebarMenuButton>
-              </Link>
-            </SidebarMenuItem>
-          ))}
+          {items.map((item) => {
+            const isActive = pathname === item.url;
+
+            return (
+              <SidebarMenuItem key={item.title}>
+                <Link
+                  href={item.url}
+                  onClick={handleItemClick}
+                  className="w-full"
+                >
+                  <SidebarMenuButton
+                    asChild
+                    className={`${
+                      isActive
+                        ? "bg-muted text-primary"
+                        : "hover:bg-muted cursor-pointer"
+                    }`}
+                  >
+                    <div className="flex items-center gap-2 w-full">
+                      <item.icon className="h-4 w-4" />
+                      <span>{item.title}</span>
+                    </div>
+                  </SidebarMenuButton>
+                </Link>
+              </SidebarMenuItem>
+            );
+          })}
         </SidebarMenu>
       </SidebarGroupContent>
     </SidebarGroup>
