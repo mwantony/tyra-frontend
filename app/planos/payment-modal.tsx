959c1bb0df0e549ea8f12bf9fa44899bb5b3169b
Antony Maziero Warken <againplayi7@gmail.com>
2025-05-02 18:03:32 -0300

    .

--- app/planos/payment-modal.tsx
+++ app/planos/payment-modal.tsx
@@ -138,8 +138,20 @@ export function PaymentModal({
               {paymentData?.status && (
                 <p className="flex items-center justify-center gap-2">
                   <span className="relative flex h-2 w-2">
-                    <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-green-500 opacity-75"></span>
-                    <span className="relative inline-flex rounded-full h-2 w-2 bg-green-500"></span>
+                    <span
+                      className={`animate-ping absolute inline-flex h-full w-full rounded-full ${
+                        paymentData.status === "pending"
+                          ? "bg-orange-500"
+                          : "bg-green-500"
+                      } opacity-75`}
+                    ></span>
+                    <span
+                      className={`relative inline-flex rounded-full h-2 w-2 ${
+                        paymentData.status === "pending"
+                          ? "bg-orange-500"
+                          : "bg-green-500"
+                      }`}
+                    ></span>
                   </span>
                   Status: {formatPaymentStatus(paymentData.status)}
                 </p>
