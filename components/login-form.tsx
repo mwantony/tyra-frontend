66ebcee03a597f6e0d0d92cf66e437df79273b4b
Antony Maziero Warken <againplayi7@gmail.com>
2025-05-04 20:39:00 -0300

    .

--- components/login-form.tsx
+++ components/login-form.tsx
@@ -99,9 +99,8 @@ export function LoginForm({
       </Card>
 
       <div className="text-balance text-center text-xs text-muted-foreground [&_a]:underline [&_a]:underline-offset-4 hover:[&_a]:text-primary">
-        Ao clicar em continuar, você concorda com nossos{" "}
-        <a href="#">Termos de Serviço</a> e{" "}
-        <a href="#">Política de Privacidade</a>.
+        Tyra - Soluções para Restaurantes | Versão 1.00 <br></br>
+        CNPJ: 00.000.000/0000-00 | Todos os direitos reservados
       </div>
     </div>
   );
