4ad1fdbb82b4997d616b9cf3c6525f71cef99636
Antony Maziero Warken <againplayi7@gmail.com>
2025-05-06 10:25:30 -0300

    .

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,12 +1,19 @@
-"use client"
-import { LoginForm } from "@/components/login-form"
+"use client";
+import { LoginForm } from "@/components/login-form";
+import { Spinner } from "@/components/ui/spinner";
+import { useAuth } from "@/contexts/auth-provider";
 
 export default function LoginPage() {
+  const { restaurante } = useAuth();
+  if (restaurante?.id) {
+    window.location.href = "/dashboard";
+    return <Spinner></Spinner>
+  }
   return (
     <div className="flex min-h-svh flex-col items-center justify-center bg-muted p-6 md:p-10">
       <div className="w-full max-w-sm md:max-w-3xl">
         <LoginForm />
       </div>
     </div>
-  )
+  );
 }
