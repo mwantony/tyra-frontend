c41b7461f7c535776a65b10354f171f1a6373419
Antony Maziero Warken <againplayi7@gmail.com>
2025-05-08 17:54:45 -0300

    .

--- app/comandas/detalhes/[numeroComanda]/payment-method-modal.tsx
+++ app/comandas/detalhes/[numeroComanda]/payment-method-modal.tsx
@@ -108,12 +108,12 @@ export function PaymentMethodDialog({
 
             <div>
               <RadioGroupItem
-                value="outro"
-                id="outro"
+                value="outros"
+                id="outros"
                 className="peer sr-only"
               />
               <Label
-                htmlFor="outro"
+                htmlFor="outros"
                 className="flex flex-col items-center justify-between rounded-md border-2 border-muted bg-popover p-4 hover:bg-accent hover:text-accent-foreground peer-data-[state=checked]:border-primary [&:has([data-state=checked])]:border-primary"
               >
                 <span className="font-medium">Outro</span>
