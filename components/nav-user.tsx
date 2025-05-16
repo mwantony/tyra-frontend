35bc589133569cbab0cdd67f7202aeef6a3e0dd0
Antony Maziero Warken <againplayi7@gmail.com>
2025-05-16 16:47:28 -0300

    .

--- components/nav-user.tsx
+++ components/nav-user.tsx
@@ -63,15 +63,15 @@ export function NavUser() {
             >
               <Avatar className="h-8 w-8 rounded-lg grayscale">
                 <AvatarFallback className="rounded-lg">
-                  {restaurante.nome_fantasia?.charAt(0).toUpperCase()}
+                  {restaurante?.nome_fantasia?.charAt(0).toUpperCase()}
                 </AvatarFallback>
               </Avatar>
               <div className="grid flex-1 text-left text-sm leading-tight">
                 <span className="truncate font-medium">
-                  {restaurante.nome_fantasia}
+                  {restaurante?.nome_fantasia}
                 </span>
                 <span className="truncate text-xs text-muted-foreground">
-                  {restaurante.email}
+                  {restaurante?.email}
                 </span>
               </div>
               <MoreVerticalIcon className="ml-auto size-4" />
@@ -87,15 +87,15 @@ export function NavUser() {
               <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
                 <Avatar className="h-8 w-8 rounded-lg">
                   <AvatarFallback className="rounded-lg">
-                    {restaurante.nome_fantasia?.charAt(0).toUpperCase()}
+                    {restaurante?.nome_fantasia?.charAt(0).toUpperCase()}
                   </AvatarFallback>
                 </Avatar>
                 <div className="grid flex-1 text-left text-sm leading-tight">
                   <span className="truncate font-medium">
-                    {limitarTexto(restaurante.nome_fantasia, 15)}
+                    {limitarTexto(restaurante?.nome_fantasia, 15)}
                   </span>
                   <span className="truncate text-xs text-muted-foreground">
-                    {restaurante.email}
+                    {restaurante?.email}
                   </span>
                 </div>
               </div>
